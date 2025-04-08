acb69ba256166dea5dd54c84acd141c8fcc64d99
MCSMDS <161939386+LMCv2@users.noreply.github.com>
2025-04-08 05:23:48 +0000

    remove service

--- src/main/java/com/aib/websystem/controller/AccountController.java
+++ src/main/java/com/aib/websystem/controller/AccountController.java
@@ -3,42 +3,42 @@ package com.aib.websystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.aib.websystem.entity.Account;
-import com.aib.websystem.service.AccountService;
+import com.aib.websystem.repository.AccountRepository;
 
 @RestController
 public class AccountController {
 
     @Autowired
-    private AccountService accountService;
+    private AccountRepository accountRepository;
 
     @GetMapping("/accounts/{id}")
     public Account getAccount(@PathVariable String id) {
-        return accountService.findAccountById(id);
+        return accountRepository.findById(id).orElse(null);
     }
 
     @PostMapping("/create-account")
-    public Account createAccount(@RequestParam String username, 
-                                @RequestParam String password, 
-                                @RequestParam String firstName, 
-                                @RequestParam String lastName) {
+    public Account createAccount(@RequestParam String username,
+            @RequestParam String password,
+            @RequestParam String firstName,
+            @RequestParam String lastName) {
         Account account = new Account();
         account.setUsername(username);
         account.setPassword(password);
         account.setFirstName(firstName);
         account.setLastName(lastName);
-        return accountService.createAccount(account);
+        return accountRepository.save(account);
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, 
-                        @RequestParam String password) {
-        Account account = accountService.login(username, password);
-        if (account != null) {
+    public String login(@RequestParam String username,
+            @RequestParam String password) {
+        Account account = accountRepository.findById(username).orElse(null);
+        if (account.getPassword().equals(password)) {
             return "Login successful for user: " + account.getUsername();
         } else {
             return "Invalid username or password";
