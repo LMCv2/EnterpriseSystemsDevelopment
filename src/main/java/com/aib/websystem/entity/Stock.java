16927e553db26d9e93fd662c4e9032bec14befe2
MCSMDS <161939386+LMCv2@users.noreply.github.com>
2025-04-12 10:31:12 +0000

    fix bug again

--- src/main/java/com/aib/websystem/entity/Stock.java
+++ src/main/java/com/aib/websystem/entity/Stock.java
@@ -1,8 +1,9 @@
 package com.aib.websystem.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +11,18 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@IdClass(StockId.class)
 @ToString
 @NoArgsConstructor
 public class Stock {
     @Id
     @Getter
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Getter
     @ManyToOne
     private Fruit fruit;
 
-    @Id
     @Getter
     @ManyToOne
     private Location location;
