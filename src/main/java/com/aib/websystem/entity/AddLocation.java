574f2667beff13217c4d161547d3ae516375e3a1
MCSMDS <161939386+LMCv2@users.noreply.github.com>
2025-05-13 15:39:19 +0000

    fix bug

--- src/main/java/com/aib/websystem/entity/AddLocation.java
+++ src/main/java/com/aib/websystem/entity/AddLocation.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @NoArgsConstructor
 public class AddLocation implements Serializable {
     @Getter
+    @Setter
     private Location location;
 
     public AddLocation(Location location) {
